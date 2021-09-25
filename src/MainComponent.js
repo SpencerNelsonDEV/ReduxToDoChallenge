e6c435d7991941fa7be4695f51983357afb7ec7f
Spencer Nelson <spencernelsondev@gmail.com>
2021-09-25 11:54:12 -0700

    02-Task-1-Complete

--- src/MainComponent.js
+++ src/MainComponent.js
@@ -1,22 +1,25 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { addTodo, clearAllTasks, toggleToDo } from './redux/ActionCreators'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { addTodo, clearAllTasks, toggleToDo } from "./redux/ActionCreators";
+import { ToDo } from "./redux/todo";
 // Task:  Import functions from ActionCreators
 
 // Task: Assign reducer to prop
 const mapStateToProps = (state) => {
-  return {}
-}
+  return {
+    todo: state.ToDo,
+  };
+};
 
 // Task: add functions to dispatch
-const mapDispatchToProps = {}
+const mapDispatchToProps = {};
 
 class Main extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      todoInput: '',
-    }
+      todoInput: "",
+    };
   }
 
   handleSubmit() {
@@ -24,40 +27,43 @@ class Main extends Component {
       // Task: add a new line to dispatch the state value to the action creator
 
       //This line doesn't change
-      this.setState({ todoInput: '' })
+      this.setState({ todoInput: "" });
     }
   }
 
   render() {
+    console.log("main render props", this.props);
     return (
-      <div className='App'>
+      <div className="App">
         <h1>Redux To Do List</h1>
         <ul>
           {/* Task: create a map that displays the list item. don't forget the unique key. we will be using the index of the array*/}
-          <li key={'unique key'}>
-            <input
-              type='checkbox'
-              // Task: replace true with the property used to show completion
-              checked={true}
-              // Task: dispatch toggle instead of console.log. Use the index of the array
-              onChange={() => console.log('Toggling')}
-            />
-            {/* Task: Replace this with task activity */}
-            {' Item'}
-          </li>
+          {this.props.todo.todo.map((todo, index) => (
+            <li key={index}>
+              <input
+                type="checkbox"
+                // Task: replace true with the property used to show completion
+                checked={todo.complete}
+                // Task: dispatch toggle instead of console.log. Use the index of the array
+                onChange={() => console.log("Toggling")}
+              />
+              {/* Task: Replace this with task activity */}
+              {todo.activity}
+            </li>
+          ))}
 
-          <div className='AddField'>
+          <div className="AddField">
             <input
-              type='text'
+              type="text"
               onChange={(e) => this.setState({ todoInput: e.target.value })}
               value={this.state.todoInput}
             />
             <div>
               <button onClick={() => this.handleSubmit()}>Add Task</button>
-              <button onClick={() => alert('Replace with dispatched function for clearing values')}>
+              <button onClick={() => alert("Replace with dispatched function for clearing values")}>
                 Remove Completed
               </button>
-              <button onClick={() => alert('Replace with dispatched function for clearing the list')}>
+              <button onClick={() => alert("Replace with dispatched function for clearing the list")}>
                 Empty List
               </button>
             </div>
@@ -67,7 +73,7 @@ class Main extends Component {
             Redux Challenge
             <br />
             <br />
-            <div style={{ justifyContent: 'center', display: 'flex' }}>
+            <div style={{ justifyContent: "center", display: "flex" }}>
               <ol>
                 <div style={{ margin: 10 }}>
                   This website is broken. We are missing some functionality and need your help fixing it.
@@ -84,9 +90,9 @@ class Main extends Component {
           </div>
         </ul>
       </div>
-    )
+    );
   }
 }
 
 // Task: Connect this function to redux
-export default connect(mapStateToProps, mapDispatchToProps)(Main)
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
